eb20850c4c5398cbc2f633c5f7d2b44e617f3071
AlexisVS <alexis.vansan1440@gmail.com>
2021-06-09 15:48:28 +0200

    base

---
+++ src/components/dfgsd.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class dfgsd extends Component {
+    render () {
+        return ( // ! si tu veut pas avoir une balise inutile dans ton html mais c'est truc chelou ce sont des React.Fragments <>...</>
+            <>
+                <div>
+
+                </div>
+                <div>
+
+                </div>
+            </>
+        );
+    }
+}
+
+export default dfgsd;
